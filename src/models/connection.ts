64cd3fdcc329c7b3d79f229bdca6df4c92301f28
furquin <laert.ff@gmail.com>
2022-05-16 16:38:47 -0300

    style: modifica nome arquivos e instala http-status

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 
 dotenv.config();
 
-const connection = mysql.createPool({
+const Connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
 });
 
-export default connection;
+export default Connection;
