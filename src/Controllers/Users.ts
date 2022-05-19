872d069d0f31245640e5c868d321634e1bd1bf90
furquin <laert.ff@gmail.com>
2022-05-19 19:45:27 -0300

    fix: corrige implementação de payload

--- src/Controllers/Users.ts
+++ src/Controllers/Users.ts
@@ -17,9 +17,11 @@ export default class UsersControllers {
     try {
       const { username, classe, level, password } = req.body;
 
+      const payload = { name: username };
+
       await this.service.create(username, classe, level, password);
       
-      const token = await this.token.createToken(username);
+      const token = await this.token.createToken(payload);
 
       return res.status(StatusCodes.CREATED).json({ token });
     } catch (e) {
